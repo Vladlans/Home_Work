0a03da140f0f899246b9a1b006b8763d82b07bce
Vlad <blackmor143@yandex.ru>
2022-10-18 18:44:02 +0300

    Выполнили 4 задание

---
+++ Zadanie_04/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+if(number <= 0)
+{
+    Console.WriteLine("Неверное число, повторите попытку");
+}
+else
+{
+    while(count <= number)
+    {
+        int reminder = count % 2;
+        if(reminder == 0)
+        {
+            Console.Write($"{count}. ");
+            count++;
+        }
+        else
+        {
+            count++;
+        }
+    }
+}
